63618ee75061a3bdcca74ef6b1ef94008bc7399c
Yuri Blinnikov <blinnikovu5@gmail.com>
2025-08-31 21:22:56 +0800

    Fix: Correct card detection logic for instant feedback

--- src/js/CardDetector.js
+++ src/js/CardDetector.js
@@ -1,11 +1,13 @@
 const cardTypes = [
+  { name: 'mir', pattern: /^220/ },
+
   { name: 'diners', pattern: /^3(?:0[0-5]|[68])/ },
   { name: 'jcb', pattern: /^35/ },
   { name: 'amex', pattern: /^3[47]/ },
   { name: 'visa', pattern: /^4/ },
-  { name: 'mir', pattern: /^220[0-4]/ },
-  { name: 'mastercard', pattern: /^(5[1-5]|2[2-7])/ },
   { name: 'discover', pattern: /^6(?:011|5)/ },
+
+  { name: 'mastercard', pattern: /^(5|2[2-7])/ },
 ];
 
 export default function getCardType(cardNumber) {
